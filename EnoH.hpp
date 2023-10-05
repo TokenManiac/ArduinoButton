e30ae9ffe19a1b3e8c23b8085596a48f6f7be53e
SyntaxMenace <93088981+SyntaxMenace@users.noreply.github.com>
2023-10-05 14:08:05 -0300

    Update EnoH.hpp

--- EnoH.hpp
+++ EnoH.hpp
@@ -1,11 +1,8 @@
 /*
 	= EnoH.hpp
 
-	? Why?
-	> Because yes!!!
-
-	? What is the point of using this?
-	> There's none, other than making buttons easier for you ;D
+	Q> What does this do?
+	A> This C++ file makes buttons easier to handle in your program, therefore easier to read.
 
 	=== REFER TO MIT LICENSE === > https://github.com/SyntaxMenace/ArduinoButton/blob/main/LICENSE
 */
@@ -14,13 +11,13 @@ class BoardButton {
 private:
 	byte Pin = 0;
 
-	byte m_DebounceTime = 50;
+	uint16_t m_DebounceTime = 50;
 
 	mutable bool m_IsPressed = false;
 	mutable uint64_t m_LastPress = 0ui64;
 public:
 	BoardButton(byte pin) : Pin(pin) {}
-	BoardButton(byte pin, byte Debounce) : Pin(pin), m_DebounceTime(Debounce) {}
+	BoardButton(byte pin, uint16_t Debounce) : Pin(pin), m_DebounceTime(Debounce) {}
 
 	bool IsPressed() const {
 		const bool ButtonState = digitalRead(Pin);
@@ -36,15 +33,44 @@ public:
 		return m_IsPressed;
 	}
 
-	void SetDebounce(byte Debounce) {
-		m_DebounceTime = Debounce;
-	}
-
 	uint64_t HeldTime() const {
 		if (this->IsPressed() == false) {
 			return 0ui32;
 		} else {
 			return static_cast<uint64_t>(millis() - m_LastPress);
 		}
+	}
+
+	void SetDebounce(uint16_t Debounce) {
+		m_DebounceTime = Debounce;
+	}
+};
+
+class ButtonSwitch {
+private:
+	bool SwitchState = 0;
+
+	uint16_t m_Debounce;
+	uint64_t m_LastPress = 0;
+public:
+	ButtonSwitch(uint16_t Debounce) :
+		m_Debounce(Debounce) {}
+	ButtonSwitch(uint16_t Debounce, bool DefaultState) :
+		m_Debounce(DefaultState),
+		SwitchState(DefaultState) {}
+
+	bool GetState() const {
+		return SwitchState;
+	}
+
+	void Toggle() {
+		if (millis() - m_LastPress >= m_Debounce) {
+			m_LastPress = millis();
+			SwitchState = !SwitchState;
+		}
+	}
+
+	uint32_t Time() const { // returns how many milliseconds passed since last toggle
+		return static_cast<uint32_t>(millis() - m_LastPress);
 	}
 };
